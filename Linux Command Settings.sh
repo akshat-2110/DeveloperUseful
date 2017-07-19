f1cde1a630155edbe3276fc093ef82c41a278ec6
Vishal Chovatiya <visheshchovatiya@gmail.com>
2017-07-19 09:47:22 +0530

    Create Linux Command Settings.sh

---
+++ Linux Command Settings.sh
@@ -0,0 +1,13 @@
+# Default to human readable figures
+ alias df='df -h'
+ alias du='du -h'
+
+# Misc :)
+ alias less='less -r'                          # raw control characters
+ alias whence='type -a'                        # where, of a sort
+ alias grep='grep --color'                     # show differences in colour
+ alias egrep='egrep --color=auto'              # show differences in colour
+ alias fgrep='fgrep --color=auto'              # show differences in colour
+
+# Some shortcuts for different directory listings
+ alias ls='ls -hltrF --color=tty'                 # classify files in colour
